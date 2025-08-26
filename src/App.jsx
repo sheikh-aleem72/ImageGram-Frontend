7ec63c965555cd135b917be60746220832da40bd
aleem <sheikhaleem363@gmail.com>
2025-08-26 15:49:29 +0530

    feat: Add logic to redirect user from authentication page to home if already login

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import { AppRoutes } from "./Routes";
 import CustomErrorBoundary from "./components/molecules/CustormErrorBoundary/CustomErrorBoundary";
 import { Toaster } from "./components/ui/sonner";
 import ModalRoot from "./components/molecules/Modal/ModalRoot/ModalRoot";
+
 function App() {
   const queryClient = new QueryClient();
+
   return (
     <QueryClientProvider client={queryClient}>
       <CustomErrorBoundary>
--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 // Layout for auth related pages
 const Auth = ({ children }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let userObject = localStorage.getItem("user");
+    if (userObject) {
+      navigate("/home");
+    }
+  }, []);
   return (
     <>
       <div className="h-[100vh] w-full flex items-center justify-center bg-imagegram-bg">
